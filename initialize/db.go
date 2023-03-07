d6747f4f87a7e87cf596b46e4910724cb57a19f7
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-03-07 13:57:14 +0000

    Connect to Database

---
+++ initialize/db.go
@@ -0,0 +1,19 @@
+package initialize
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	fmt.Println("Database connected!")
+	return db
+}
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/arshad-siddiqui/sidequest/initialize"
 	"github.com/gofiber/fiber/v2"
 )
 
 func init() {
 	initialize.LoadEnv()
+	initialize.ConnectDB()
 }
 func main() {
 	app := fiber.New()
@@ -15,5 +19,5 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 	app.Listen(":3000")
-
+	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
