0d6bac2a709451a389c04ae3649751fba2f000e5
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-03-22 22:55:03 +0000

    Refactor addUser to modify DB directly

--- controllers_test/utility_test.go
+++ controllers_test/utility_test.go
@@ -1,32 +1,15 @@
 package controllers_test
 
 import (
-	"bytes"
-	"encoding/json"
-	"net/http"
-
-	"github.com/arshad-siddiqui/sidequest/controllers"
 	. "github.com/arshad-siddiqui/sidequest/initialize"
+	"github.com/arshad-siddiqui/sidequest/models"
 	"github.com/arshad-siddiqui/sidequest/reset"
-	"github.com/gofiber/fiber/v2"
 )
 
 func addUser(email, password string) {
-	initDB()
+	user := models.User{Email: email, Password: password}
+	DB.Create(&user)
 
-	url := "/user/create"
-	app := fiber.New()
-	app.Post(url, controllers.UserCreate)
-
-	values := map[string]string{"email": email, "password": password}
-
-	jsonValue, _ := json.Marshal(values)
-
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
-
-	req.Header.Set("Content-Type", "application/json")
-
-	app.Test(req)
 }
 
 func initAndResetDB() {
